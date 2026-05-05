d4fdbcad4db80261c0b71937d51d4182285d8661
CatWorldOfficial <catworldbyanokhi@gmail.com>
2026-05-05 18:52:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+name="Anokhi Parihar"
+print("Hello I am",name)
+age=10
+print("I am",age,"years old.")
+weight=32.8
+print("My weight is",weight,"kgs")
+is_student=True
+print("Are you a student?",is_student)
---
+++ main2.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=a+b
+print("The sum is:",c)
+d=a-b
+print("The difference is:",d)
+e=a*b
+print("The product is:",e)
+f=a//b
+print("The quiotient is:",f)
+g=a%b
+print("The remainder is:",g)
